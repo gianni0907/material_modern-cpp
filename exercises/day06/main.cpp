3a594e972d6bc3eb6d326e6cf63f8a78bda54156
Giovanbattista Gravina <gianba.gravina7@gmail.com>
2025-05-16 14:44:21 +0000

    day06 exercise solution

--- exercises/day06/main.cpp
+++ exercises/day06/main.cpp
@@ -18,6 +18,7 @@ int main(int argc, char *argv[])
         *d = 2.0;
         (*pt).X = 3.0; // Modify x coordinate
         (*pt).Y = 3.0; // Modify y coordinate
+        std::cout << *d << std::endl;
         std::cout<<pt->X<<" "<<pt->Y<<std::endl;
         // Can use operators for pointer operations
         pt->X = 5.0; // Modify x coordinate
@@ -30,6 +31,18 @@ int main(int argc, char *argv[])
 
     {
         // TODO: Rewrite the above code using smart pointers
+        auto d = std::make_unique<double>(1.0);
+        auto pt = std::make_unique<Point>(1.0, 2.0);
+        auto p {std::make_unique<Point>(10.0, 13.0)};
+        std::cout<<p->X<<" "<<p->Y<<std::endl;
+        *d = 2.0;
+        pt->X = 3.0;
+        pt->Y = 3.0;
+        std::cout << *d << std::endl;
+        std::cout<<pt->X<<" "<<pt->Y<<std::endl;
+        pt->X = 5.0;
+        pt->Y = 6.0;
+        std::cout<<pt->X<<" "<<pt->Y<<std::endl;
     }
 
     {
@@ -56,6 +69,24 @@ int main(int argc, char *argv[])
     {
         // TODO: Rewrite the above code using smart pointers
         // Bonus: inspect the p.use_count() of the shared_ptr
+        auto d = std::make_unique<double>(1.0);
+        auto pt1 = std::make_shared<Point>(1.0, 2.0); // Two-d Point class
+        std::cout<<pt1.use_count()<<" "<<std::endl;
+        auto pt2 = pt1;
+        std::cout<<pt1.use_count()<<" "<<pt2.use_count()<<std::endl;
+        // Dereference and call member functions
+        *d = 2.0;
+        (*pt1).X = 3.0; // Modify x coordinate
+        (*pt1).Y = 3.0; // Modify y coordinate
+        std::cout<<pt2->X<<" "<<pt2->Y<<std::endl;
+        // dereference pt1
+        std::cout<<pt1.use_count()<<" "<<pt2.use_count()<<std::endl;
+        pt1 = nullptr;
+        std::cout<<pt1.use_count()<<" "<<pt2.use_count()<<std::endl;
+        pt2->X = 5.0; // Modify x coordinate
+        pt2->Y = 6.0; // Modify y coordinate
+        std::cout<<pt2->X<<" "<<pt2->Y<<std::endl;
+
     }
 
 }
